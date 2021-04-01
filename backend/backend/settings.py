fc34cf9ffa1082b591c9a63d2622c39477d06613
PayasSingh <payas@ualberta.ca>
2021-03-31 18:39:55 -0600

    add models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'entrec.apps.EntrecConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
