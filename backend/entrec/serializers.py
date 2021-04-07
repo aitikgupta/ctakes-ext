b662b51174cabb401e9ba48253e11d97b8bc70ec
Shivam <shivam.318b@gmail.com>
2021-04-07 21:10:23 +0530

    Populate Document Serializer Class

--- backend/entrec/serializers.py
+++ backend/entrec/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
-from entrec.models import Document, Text, Information
+from entrec.models import Document
 
 class EntRecSerializer(serializers.Serializer):
     # serializers for Web API
+    Document.id = serializers.IntegerField(read_only=true)
+    Document.document_name = serializers.CharField(required=True, allow_blank=False, max_length=50)
+    Document.submission_date = serializers.DateField()
+    Document.submission_time = serializers.TimeField()
+
 
     def create(self, validated_data):
         """
-        Create and return a new 'Snippet' instance, given the validated data.
+        Create and return a new 'entrec' instance, given the validated data.
         """
         return Document.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
         Update and return an existing 'entrec' instance, given the validated data.
+        """
+        instance.document_name = validated_data.get('document name', instance.document_name)
+        instance.document_date = validated_data.get('document date submitted', instance.document_name)
+        instance.document_name = validated_data.get('document date submitted', instance.document_name)
+        instance.save()
+        return instance
